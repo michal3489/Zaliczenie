1a6b4f596c9d50d913c6e2e7d5ec192b94229064
AdamOsmanski <adamosmanski1@gmail.com>
2020-04-19 18:41:28 +0200

    Utworzenie wielokątów
    Utworznie wielokątów oraz ich wypełenie. Małe zmiany w klasie "rodzica

--- zaliczenie1/Bazowa.h
+++ zaliczenie1/Bazowa.h
@@ -1,22 +1,21 @@
+#include <iostream>
 #include <string>
+
+#include <cmath>
 using namespace std;
 class Bazowa
 {
-		//zmmienne
+protected:
+	string nazwa;
+	float pole;
+	float obwod;
+	float objetosc;
+	float bok1, bok2, bok3, bok4;
+	Bazowa(string n = "", float p = 0, float ob = 0, float obj =0, float b1 = 0, float b2 = 0, float b3 = 0, float b4 = 0);
+	virtual ~Bazowa();
 public:
-		string nazwa;
-		float pole;
-		float obwod;
-		float bok1, bok2, bok3, bok4;
-
-
-		//konstruktor i destruktor
-		Bazowa(string n="", float p=0, float ob=0, float b1=0, float b2=0, float b3=0, float b4=0);
-		virtual ~Bazowa();
-
-		//metody
-		void policzpole();
-		void policzobwod();
+	void wyswietl();
+	void objetosc();
 };
 
 
