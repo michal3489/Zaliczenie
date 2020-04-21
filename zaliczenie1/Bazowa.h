5bf3d310de58a0228a27ea1ba2e5fb2590bb7857
michal3489 <61480643+michal3489@users.noreply.github.com>
2020-04-21 18:01:48 +0200

    Wprowadzenie nowych wzorów liczenia pola

--- zaliczenie1/Bazowa.h
+++ zaliczenie1/Bazowa.h
@@ -8,10 +8,10 @@ public:
 		float pole;
 		float obwod;
 		float bok1, bok2, bok3, bok4;
-
+		float pp, pb;
 
 		//konstruktor i destruktor
-		Bazowa(string n="", float p=0, float ob=0, float b1=0, float b2=0, float b3=0, float b4=0);
+		Bazowa(string n="", float pb = 0,float pp=0, float p=0, float ob=0, float b1=0, float b2=0, float b3=0, float b4=0);
 		virtual ~Bazowa();
 
 		//metody
--- zaliczenie1/Figura.h
+++ zaliczenie1/Figura.h
@@ -5,5 +5,27 @@ using namespace std;
 
 class Figura:protected Bazowa
 {
+	void policzpole()
+	{
+		this->pole = p1;
+		this->bok1 = b1;
+		this->bok2 = b2;
+		this->bok3 = b3;
+		this->bok4 = b4;
+		this->pp = pp;
+		this->pb = pb;
+		if (b1*b2 > 0) {
+			pp = (b1 * b2) * 2;
+			pb = 4 * (b3 * b4);
+			p1 = pp + pb;
+			printf("pole figury to %f" + sp1)
+		}
+		else: 
+		prinf("B³¹d nie mo¿na policzyæ pola bez pola podstawy");
+	}
+	void policzobwod()
+	{
+
+	}
 	
 };
