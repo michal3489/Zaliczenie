af2cd72a27ce11973be6024e030423609741736b
michal3489 <61480643+michal3489@users.noreply.github.com>
2020-04-25 12:50:21 +0200

    Podstawy do liczenia pola, obwodu i przekatnej dla brył

--- zaliczenie1/Figura.h
+++ zaliczenie1/Figura.h
@@ -1,9 +1,38 @@
 #include <iostream>
+#include <string>
+
+#include <cmath>
 #include "Bazowa.h"
 
 using namespace std;
 
-class Figura:protected Bazowa
+class Szescian: Bazowa
 {
-	
+	void pole();
+	void obwod();
+	void przekatna();
+};
+class Prostopadloscian: Bazowa
+{
+	void pole();
+	void obwod();
+	void przekatna();
+};
+class Graniastoslup: Bazowa
+{
+	void pole();
+	void obwod();
+	void przekatna();
+};
+class Stozek: Bazowa
+{
+	void pole();
+	void obwod();
+	void przekatna();
+};
+class Ostroslup: Bazowa
+{
+	void pole();
+	void obwod();
+	void przekatna();
 };
